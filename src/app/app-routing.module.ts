34d3f6015dae431c7800317b4b6688dd9b515b9d
root <root@DESKTOP-88U0KEK>
2023-05-30 13:29:31 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LocationComponent } from './location/location.component';
+import { HistoryComponent } from './history/history.component';
+import { PrivateComponent } from './private/private.component';
+import { GuardService } from './services/guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "location", component: LocationComponent },
+  { path: "history", component: HistoryComponent },
+  { path: "private", component: PrivateComponent, canActivate: [GuardService]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
